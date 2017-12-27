35375038e48ebeeb3e8f231b88902d46c7a0128d
Unknown <andreasblstd@gmail.com>
2017-12-27 14:08:37 +0100

    Correct user admin interface

--- user/admin.py
+++ user/admin.py
@@ -7,12 +7,11 @@ from django.utils.translation import ugettext, ugettext_lazy as _
 class UserFieldAdmin(UserAdmin):
     fieldsets = (
         (None, {'fields': ('username', 'password')}),
-        (_('Personal info'), {'fields': ('first_name', 'last_name', 'email', 'allergies')}),
+        (_('Personal info'), {'fields': ('first_name', 'last_name', 'email', 'starting_year', 'allergies')}),
         (_('Permissions'), {'fields': ('is_active', 'is_staff', 'is_superuser',
                                        'groups', 'user_permissions')}),
         (_('Important dates'), {'fields': ('last_login', 'date_joined')}),
         ('Brukerstatus', {'fields': ('is_awaiting_approval','account_verified')}),
-        ('Årskull', {'fields': ('starting_year',)}),
     )
 
 
