340a890477ee680635c9c1d0fd693dea6b8568e5
Thorvald Ballestad <thorvald.tb@gmail.com>
2018-03-06 19:13:29 +0100

    Includes flatpage-urls and end of urls.py

--- jubside/urls.py
+++ jubside/urls.py
@@ -34,6 +34,7 @@ urlpatterns = [
     # Del filer (Husk manage.py collectstatic for static filer når DEBUG=False)
     url(r'^media/(?P<path>.*)$', serve, {'document_root': settings.MEDIA_ROOT}),
     url(r'^static/(?P<path>.*)$', serve, {'document_root': settings.STATIC_ROOT}),
+    url(r'^', include('django.contrib.flatpages.urls')),
 
 ]
 
