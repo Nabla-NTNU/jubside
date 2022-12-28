4e6888b3450e047307435c010c079e4d67d8f083
Amund Eggen Svandal <aesvandal@gmail.com>
2022-12-28 17:44:18 +0100

    Get secret key from env

--- jubside/settings/production.py
+++ jubside/settings/production.py
@@ -32,7 +32,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/1.11/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'ff34xtnmhg8uc3er$=q@y1lk2w^*m^ta7(1qf0diz#hbw62ays'
+SECRET_KEY = get_env("SECRET_KEY")
 
 
 LOGIN_REDIRECT_URL = '/min-profil/'
