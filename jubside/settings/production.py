e8c71b3872f5077fd1ee65e3aaa130801cb98c0d
Amund Eggen Svandal <aesvandal@gmail.com>
2023-01-27 02:30:31 +0100

    Add pymysql version hack

--- jubside/settings/production.py
+++ jubside/settings/production.py
@@ -15,6 +15,13 @@ import os
 import pymysql
 from easy_thumbnails.conf import Settings as EasyThumbnailSettings
 
+pymysql.version_info = (
+    1,
+    4,
+    2,
+    "final",
+    0,
+)  # PyMySQL needs to lie about its version for django>=2.2
 pymysql.install_as_MySQLdb()
 get_env = os.environ.get
 
